f026a97cd22308643dbb68d6dff90e428a987d0e
Sarah Wimbish <smwimbish@verizon.net>
2022-06-12 15:43:32 -0400

    meaningful about and home changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
+import "./About.css";
 import profile_image from "../assets/profile_image.jpg";
   
 export default class About extends Component {
   render() {
     return (
-    <div>
       <div>
         <div class="split left">
           <div className="centered">
             <img 
               className="profile_image"
-              profile_image
+              src = {profile_image}
               alt="Profile Pic"
               ></img>
           </div>
@@ -24,7 +24,6 @@ export default class About extends Component {
           </div>
         </div>
       </div>
-    </div>
     )
   }
 }
